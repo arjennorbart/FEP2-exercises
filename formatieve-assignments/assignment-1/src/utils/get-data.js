2f55b95e6762f2ec8923d5b24d1973d0dd096f62
Sanne Veroude <sanne.veroude@hu.nl>
2020-10-26 10:16:55 +0100

    moved to other repo

--- formatieve-assignments/assignment-1/src/utils/get-data.js
+++
@@ -1,4 +0,0 @@
-export default function getData(path) {
-  return fetch(`/data/${path}.json`)
-    .then(res => res.json())
-}
--- formatieve-assignments/assignment-3/src/utils/get-selected-option-values.js
+++
@@ -1,5 +0,0 @@
-export default function getSelectedOptionsValueFromSelect(selectElement) {
-  return Array.from(selectElement.options)
-      .filter(option => option.selected)
-      .map(option => option.value)
-}
